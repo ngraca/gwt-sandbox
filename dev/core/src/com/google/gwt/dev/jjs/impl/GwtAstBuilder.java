5e09cc13ee7cab0641b9e87af9d5469b3a6373ae
cromwellian@gmail.com <foobar99>
2013-10-02 12:16:27 -0700

    working

--- dev/core/src/com/google/gwt/dev/jjs/impl/GwtAstBuilder.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/GwtAstBuilder.java
@@ -3474,6 +3474,9 @@ public class GwtAstBuilder {
       }
       name = intern(name);
       JDeclaredType type;
+        if (name.contains("Defender") || name.contains("Foo")) {
+            boolean xx = true;
+        }
       if (binding.isClass()) {
         type = new JClassType(info, name, binding.isAbstract(), binding.isFinal());
       } else if (binding.isInterface() || binding.isAnnotationType()) {
