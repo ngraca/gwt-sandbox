2dc59e8af1f10706da1b3b68dc0164127f31f44e
cromwellian@gmail.com <foobar99>
2013-10-05 01:11:25 -0700

    Fix autoboxing for method reference expressions, add return statement on lambda bodies

--- dev/core/src/com/google/gwt/dev/jjs/JavaToJavaScriptCompiler.java
+++ dev/core/src/com/google/gwt/dev/jjs/JavaToJavaScriptCompiler.java
@@ -703,7 +703,9 @@ public class JavaToJavaScriptCompiler {
         }
       }
 
-      // (3) Perform Java AST normalizations.
+      MixinDefenderMethods.exec(jprogram);
+
+        // (3) Perform Java AST normalizations.
       FixAssignmentsToUnboxOrCast.exec(jprogram);
 
       /*
@@ -726,7 +728,6 @@ public class JavaToJavaScriptCompiler {
       }
 
       ImplementClassLiteralsAsFields.exec(jprogram);
-      MixinDefenderMethods.exec(jprogram);
 
       /*
        * 4) Possibly optimize some.
